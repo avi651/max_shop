7e44cb807baab0d16090606d7e568043865884c1
Avinash <kumaravi651@gmail.com>
2020-07-05 21:42:14 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/max_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.max_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
